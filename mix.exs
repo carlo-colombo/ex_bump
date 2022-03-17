c0004c997494eff7a8330eae42409a7869e1590c
Carlo Colombo <carlo.colombo@gmx.com>
2022-03-17 23:10:17 +0100

    relaxed elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExBump.Mixfile do
   def project do
     [app: :bump,
      version: "0.1.0",
-     elixir: "1.0.3",
+     elixir: "*",
      description: description,
      package: package,
      deps: deps]
